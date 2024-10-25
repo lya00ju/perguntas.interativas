f22980a4f7a8de457f741754d6bbafd625421206
lya00ju <133046811+lya00ju@users.noreply.github.com>
2024-10-25 10:42:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Culinária',
+    'Qual é a única comida que nunca estraga?',
+    'O mel.'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual filme de terror foi a estreia de Johnny Depp?',
+    'A Hora do Pesadelo.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o lugar mais seco do mundo?',
+    'O deserto do Atacama.'
+)
+
+criaCartao(
+    'Biologia',
+    'Quantos ossos temos no nosso corpo?',
+    '206 ossos.'
+)
+
+criaCartao(
+    'História',
+    'Qual rei ou rainha ocupou por mais tempo o trono do Reino Unido?',
+    'Rainha Elizabeth II.'
+)
+
+criaCartao(
+    'Esporte',
+    'Quantas vezes o Brasil foi campeão da Copa do Mundo?',
+    '5 vezes.'
+)
+
+criaCartao(
+    'Química',
+    'De que são constituídos os diamantes?',
+    'Carbono.'
+)
+
+criaCartao(
+    'Biologia',
+    'Quantos corações tem um polvo?',
+    '3 corações.'
+)
+
+criaCartao(
+    'Esporte',
+    'Quantas medalhas olímpicas Michael Phelps conquistou?',
+    '28 medalhadas.'
+)
